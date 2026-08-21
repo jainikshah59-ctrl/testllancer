4f2e134859091dd797964791fdb53382c1afa5c5
jainikshah59-ctrl <jainikshah59@gmail.com>
2026-08-21 10:16:31 +0530

    Load mobile hero layout fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './polish.css'
 import './site-polish.js'
+import './mobile-hero.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
